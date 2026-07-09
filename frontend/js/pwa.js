3d0b3d3a4bd7694fc2cc7f5d049d679a951c27be
pioterdrozdo-ctrl <pioter.drozdo@yandex.ru>
2026-07-10 01:06:28 +0300

    style: load profile badge hide css

--- frontend/js/pwa.js
+++ frontend/js/pwa.js
@@ -90,6 +90,7 @@
         loadStylesheet('css/book-detail-polish.css?v=20260709-book-detail-scroll-1', '__bibliotechBookDetailPolishCss');
         loadStylesheet('css/book-detail-hide-meta.css?v=20260710-hide-inner-badges-1', '__bibliotechBookDetailHideMetaCss');
         loadStylesheet('css/book-detail-desktop-compact.css?v=20260710-inner-card-refined-1', '__bibliotechBookDetailDesktopCompactCss');
+        loadStylesheet('css/profile-hide-theme-badge.css?v=20260710-hide-theme-badge-1', '__bibliotechProfileHideThemeBadgeCss');
     }
 
     if (isHomePage) {
