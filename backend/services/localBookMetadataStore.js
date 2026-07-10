22f5fd44d4e549d47bb87b3262e7b21146b78b3d
pioterdrozdo-ctrl <pioter.drozdo@yandex.ru>
2026-07-10 14:42:40 +0300

    fix: apply metadata-aware JSON catalog search

--- backend/services/localBookMetadataStore.js
+++ backend/services/localBookMetadataStore.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const localStore = require('./localStore');
+const localStore = require('./registerBookMetadataFallback');
 const { normalizeBookMetadataInput } = require('./isbnMetadata');
 
 function readStore() {
