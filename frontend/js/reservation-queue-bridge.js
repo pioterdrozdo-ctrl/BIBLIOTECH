249ca5afc163d138c2e72144582a49782eff7307
pioterdrozdo-ctrl <pioter.drozdo@yandex.ru>
2026-07-10 15:39:28 +0300

    chore: remove unused reservation bridge

--- frontend/js/reservation-queue-bridge.js
+++
@@ -1,9 +0,0 @@
-(function installReservationQueueBridge() {
-    const controller = window.BibliotechReservationQueue;
-    if (!controller?.performReservationAction) {
-        window.setTimeout(installReservationQueueBridge, 20);
-        return;
-    }
-    window.toggleBookRental = controller.performReservationAction;
-    try { toggleBookRental = controller.performReservationAction; } catch {}
-})();
